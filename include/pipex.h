66f154eb4e78caf651f807382c59edd4664c9b00
Eliott Vallee dore <evallee-@c1r5p5.42quebec.com>
2023-04-21 00:17:51 -0400

    aaaaaa im coomiting/

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 08:52:18 by evallee-          #+#    #+#             */
-/*   Updated: 2023/04/17 19:24:43 by evallee-         ###   ########.fr       */
+/*   Updated: 2023/04/20 20:10:10 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,13 @@
 # include <unistd.h>
 # include <sys/wait.h>
 # include "libft.h"
+
+typedef struct pipex
+{
+	int		fd[2];
+	int		pid[2];
+	char	paths[2];
+	char	**cmds[2];
+}	t_pipex;
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 08:52:06 by evallee-          #+#    #+#             */
-/*   Updated: 2023/04/18 17:41:37 by evallee-         ###   ########.fr       */
+/*   Updated: 2023/04/21 00:15:50 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,43 +46,67 @@
 	waitpid(pid[1], NULL, 0);
 */
 
-void	free_cmd(char	**cmd)
+/*void	pipex(const char *in, char **cmd1, char **cmd2, const char *out)
 {
+	
+}*/
+
+void	exec_cmd(t_pipex *pipex, int std, char *arg)
+{
+	char	**cmd;
 	char	**arr;
 
+	cmd = ft_split(arg, ' ');
+	dup2(pipex->fd[std], std);
+	close(pipex->fd[1 - std]);
+	dup2(pipex->paths[1 - std], 1 - std);
+	execve(cmd[0], &cmd[1], NULL);
 	arr = cmd;
 	while (*arr)
 		free(*arr++);
 	free(cmd);
 }
 
-void	exec_cmd(char	**cmd)
+int	open_inout(t_pipex *pipex, char	*in, char *out)
 {
-	uint32_t	i;
-
-	i = 0;
-	while (cmd[i])
-		ft_printf("%s\n", cmd[i++]);
-	free_cmd(cmd);
+	pipex->paths[0] = open(in, O_RDONLY);
+	if (pipex->paths[0] < 0)
+		return (EXIT_FAILURE);
+	pipex->paths[1] = open(out, O_TRUNC | O_CREAT | O_RDWR, 0000644);
+	if (pipex->paths[1] < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-int	main(int argc, char	**argv)
+void	close_pipe(t_pipex *pipex)
 {
-	char	*in_path;
-	char	*out_path;
-	char	*cmd1;
-	char	*cmd2;
+	close(pipex->fd[0]);
+	close(pipex->fd[1]);
+	waitpid(pipex->pid[0], NULL, 0);
+	waitpid(pipex->pid[1], NULL, 0);
+}
 
-	if (argc == 5)
-	{
-		in_path = argv[1];
-		ft_printf("in: %s\n", in_path);
-		cmd1 = argv[2];
-		exec_cmd(ft_split(cmd1, ' '));
-		cmd2 = argv[3];
-		exec_cmd(ft_split(cmd2, ' '));
-		out_path = argv[4];
-		ft_printf("out: %s\n", out_path);
-	}
+int	main(int argc, char	**argv, char **env)
+{
+	t_pipex	pipex;
+
+	(void)env;
+	if (argc != 5)
+		return (EXIT_FAILURE);
+	if (open_inout(&pipex, argv[1], argv[argc - 1]) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (pipe(pipex.fd) == -1)
+		return (EXIT_FAILURE);
+	pipex.pid[0] = fork();
+	if (pipex.pid[0] < 0)
+		return (2);
+	if (pipex.pid[0] == 0)
+		exec_cmd(&pipex, 1, argv[2]);
+	pipex.pid[1] = fork();
+	if (pipex.pid[1] < 0)
+		return (3);
+	if (pipex.pid[1] == 0)
+		exec_cmd(&pipex, 0, argv[3]);
+	close_pipe(&pipex);
 	return (EXIT_SUCCESS);
 }
